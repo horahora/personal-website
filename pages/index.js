7be3a07071de20e24dabc592744fb963b51ef991
horahora <contact@horahora.com>
2022-06-17 09:53:09 +0800

    fix: upload missing file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Card from "../components/Card.js";
+import Card from "../components/Card";
 import Link from "next/link";
 import styles from "./Home.module.css";
 import React, { useEffect } from "react";
