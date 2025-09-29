a5586885277207d899308870ce5cdbdce5b8011d
horahora <contact@horahora.com>
2025-09-29 14:26:34 +0800

    fix: uncomment webpack configuration for SVG handling

--- next.config.ts
+++ next.config.ts
@@ -14,14 +14,14 @@ const nextConfig: NextConfig = {
       },
     },
   },
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: ["@svgr/webpack"],
-  //   });
-  //   return config;
-  // },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
