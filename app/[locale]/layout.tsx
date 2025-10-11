c84f3c4c9cb449554bdf149561f595a6442820e7
horahora <contact@horahora.com>
2025-10-11 20:30:57 +0800

    feat: add static params generation in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,9 @@
 import type { Viewport, Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
 import { SITE_NAME } from "@/constants";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { routing } from "@/i18n/routing";
 import "./globals.css";
 
 export const viewport: Viewport = {
@@ -14,6 +16,12 @@ export const metadata: Metadata = {
   title: SITE_NAME,
 };
 
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
 export default async function RootLayout({
   children,
   params,
@@ -22,6 +30,10 @@ export default async function RootLayout({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
   return (
     <html lang={locale}>
       <body className="antialiased min-w-[320px]">
